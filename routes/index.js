7091397c5d61248484ea2faf389eaad6f5184db4
Rob Tong <robtong21@gmail.com>
2017-03-13 15:53:46 -0500

    added section 5

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,17 @@
 const express = require('express');
 const router = express.Router();
 const wikiRouter = require('./wiki.js');
-
+const models = require('../models');
+const Page = models.Page;
 
 router.use('/wiki', wikiRouter) // move this to routes
 
 
 router.get('/', function(req, res, next) {
-    res.render('index')
+     Page.findAll()
+     .then(function(result) {
+        res.render('index', {result})
+    })
 })
 
 module.exports = router;
--- routes/wiki.js
+++ routes/wiki.js
@@ -6,10 +6,12 @@ const models = require('../models');
 const Page = models.Page;
 
 router.get('/', function(req, res){
-   res.redirect('/');
+    res.redirect('/');
 });
 
-router.post('/', function(req, res){
+router.post('/', function(req, res, next){
+    const name = req.body.name;
+    const email = req.body.email;
     const title = req.body.title;
     const content = req.body.content;
     const status = req.body.status;
@@ -23,14 +25,29 @@ router.post('/', function(req, res){
     });
     page.save()
     .then(function(obj){
-        res.json(obj)
+        res.redirect(obj.route);
     })
+    .catch(next)
 });
 
 router.get('/add', function(req, res){
     res.render('addpage')
 })
 
+router.get('/:urltitle', function(req, res, next) {
+    Page.findAll({
+        where: {
+            urlTitle: req.params.urltitle
+        }
+    })
+    .then(function(obj) {
+        // res.send(obj);
+        res.render('wikipage', {obj})
+    })
+    .catch(next);
+})
+
+// [{"route":"/wiki/nicks_page","id":1,"title":"nick's page","urlTitle":"nicks_page","content":"faj;slajfl;saj\r\nEnter text here...","status":"open","date":"2017-03-13T19:54:23.000Z","createdAt":"2017-03-13T19:54:23.349Z","updatedAt":"2017-03-13T19:54:23.349Z"}]
 
 
 
